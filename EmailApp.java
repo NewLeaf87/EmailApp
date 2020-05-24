fa9b92a399bb17101bb8d305964d1b65556cef10
larell mcbride <mcbridelarell@gmail.com>
2020-05-24 19:39:18 -0400

    Final commit

---
+++ EmailApp.java
@@ -0,0 +1,12 @@
+package EmailApp;
+
+public class EmailApp {
+ 
+    public static void main(String[] args) {
+        //???
+        Email email1 = new Email ("Naruto", "Uzumaki");
+        System.out.println(email1.showInfo());
+        
+    }
+    
+}
